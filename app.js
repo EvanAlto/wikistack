2232bb01b200281530569c8c54a394ca39a9306c
EvanAlto <altoevanb@gmail.com>
2018-11-09 11:01:04 -0600

    brought in views, models, connected to db, and MORE

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const layout = require('./views/layout');
+const { db } = require('./models');
+
+db.authenticate().then(() => {
+    console.log('db is authenticated');
+})
 
 app.use(morgan("dev"));
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', (req,res,next) => {
-    res.send('<h1>HELLO</h1>');
+    res.send(layout(''));
 })
 
 const PORT = 1337;
 app.listen(PORT,()=>{
-    console.log(PORT,' is up.');
+    console.log(`server on ${PORT} is a go.`);
 })
