a22af1771a052817d4781e3277681d1966b0ac74
Kevin Tun <kevintun01@gmail.com>
2018-11-09 11:49:43 -0600

    added sync

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const layout = require('./views/layout');
-const { db } = require('./models');
+const { Page, User, db } = require('./models');
 
-db.authenticate().then(() => {
-    console.log('db is authenticated');
-})
+const PORT = 1337;
 
+
+// db.authenticate().then(() => {
+    //     console.log('db is authenticated');
+    // })
+    
 app.use(morgan("dev"));
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(__dirname + "/public"));
@@ -16,7 +19,11 @@ app.get('/', (req,res,next) => {
     res.send(layout(''));
 })
 
-const PORT = 1337;
-app.listen(PORT,()=>{
-    console.log(`server on ${PORT} is a go.`);
-})
+const synchronize = async () => {
+    await db.sync({force : true});
+    app.listen(PORT,()=>{
+        console.log(`server on ${PORT} is a go.`);
+    })
+}
+
+synchronize();
--- models/index.js
+++ models/index.js
@@ -1,6 +1,27 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/wikistack');
+const db = new Sequelize('postgres://localhost:5432/wikistack', {
+    logging: true
+});
+
+const User = db.define('user', {
+    name: Sequelize.STRING,
+    email: Sequelize.STRING
+})
+
+const Page = db.define('page', {
+    title: Sequelize.STRING,
+    slug: Sequelize.STRING,
+    content: Sequelize.TEXT,
+    status: Sequelize.ENUM('open', 'closed')
+    
+})
+
+
+
+
 
 module.exports = {
+    Page,
+    User,
     db
 }
